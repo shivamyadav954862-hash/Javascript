b505d3f6ffb12df7ee91fb639872771b3c48ebe0
Shivam yadav <shivamyadav984862@gmail.com>
2026-03-23 14:33:29 +0530

    string

---
+++ Bacies/Conditionals if,else-if/conversion,operation/text.js
@@ -0,0 +1,62 @@
+//  let score  = 33
+//  console.log(typeof score)
+
+// let valueInNumber = Number(score)
+// console.log(typeof valueInNumber)
+// console.log(valueInNumber)
+
+
+
+//"33" => 33
+//"33abc" => NaN
+// "true" => 1; false => 0;
+
+
+
+// let isLoggedIn = 1
+
+// let booleanIsLoggedIn = Boolean(isLoggedIn)
+// console.log(booleanIsLoggedIn);
+
+
+// 1 = true ; 0 = false;
+// "" = false;
+// "Shivam" = true;
+
+
+let = someNumber = 33
+
+let stringNumber = String(someNumber)
+//console.log(stringNumber);
+//console.log(typeof stringNumber);
+
+
+//Number() → convert to number
+//String() → convert to string
+//Boolean() → convert to true/false
+
+
+
+
+//****************************conversion*******************************
+
+
+let value = 3
+let negvalue = -value
+//console.log(negvalue)
+
+
+let str1 = "hello"
+let str2 = "Shivam"
+str3 = str1 + str2
+// console.log(str3)
+
+
+// console.log("1"+ 2);
+// console.log(1+ "2");
+// console.log("1"+ 2 + 2);               string start → sab string    
+// console.log(1 + 2 + "2");              pehle number add → phir string
+
+
+
+
---
+++ Bacies/loops/string.js
@@ -0,0 +1,28 @@
+const name = "Shivam"
+const repoCount = 50
+
+// console.log(name + repoCount + "value")
+
+console.log(`Hello my name is ${name} and my repo count is ${repoCount}`);
+
+const gameName = new String('shivaam')
+
+console.log(gameName.length);
+console.log(gameName.charAt(2));
+console.log(gameName.indexOf('i'));
+console.log(gameName.toUpperCase());
+
+let str1 = "Shivam"
+console.log(str1.slice(0,3)) // Shi
+
+let str2 = "Hello bhai"
+console.log(str2.replace("bhai", "Shivam"))
+
+let str3 = "   hi   "
+console.log(str3.trim()) // "hi"
+
+let str = "a,b,c"
+console.log(str.split(",")) 
+
+let str4 = "bhai bhai"
+console.log(str4.replaceAll("bhai", "Shiv"))
