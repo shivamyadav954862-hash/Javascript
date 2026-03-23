fec3c6ed2d0784ea440511efbfeaf029a9663092
Shivam yadav <shivamyadav984862@gmail.com>
2026-03-23 13:13:47 +0530

    datatype summary

---
+++ variables/Conditionals if,else-if/datatype.js
@@ -0,0 +1,42 @@
+// Primitive
+
+// 7 types : String, Number, Boolean , null , undefined, Symbol, BigInt
+
+
+
+// Reference  (non primitive)
+
+// Array,Objects,functions
+
+// const heros = ["saktiman,krish,naagraj"]
+ let myObj = {
+    name: "Shivam",
+    age : 22,
+}
+
+
+const myFunction = function(){
+    comsole.log("Hello world")
+}
+
+
+
+
+
+// Return type of variables in JavaScript
+// =======================
+//  Primitive Datatypes
+// ---------------------------------------------------
+//        Number =>     number
+//        String  =>        string
+//        Boolean  =>    boolean
+//        null  =>             object
+//        undefined  =>  undefined
+//        Symbol  =>      symbol
+//        BigInt  =>         bigint
+// ========================
+//  Non-primitive Datatypes
+// ---------------------------------------------
+//        Arrays  =>       object
+//        Function  =>  function
+//        Object  =>       object
