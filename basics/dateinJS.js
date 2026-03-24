e8bd130391fe30c401d6f462195fac0571062934
Shivam yadav <shivamyadav984862@gmail.com>
2026-03-24 10:52:19 +0530

    Dates

---
+++ basics/dateinJS.js
@@ -0,0 +1,25 @@
+//Dates
+
+let myDate  = new Date()
+   // console.log(myDate.toString());
+
+
+//     console.log(myDate.getFullYear()) // year
+// console.log(myDate.getMonth())    // 0–11 
+// console.log(myDate.getDate())     // day
+// console.log(myDate.getDay())      // 0–6 (Sun–Sat)
+
+
+    // console.log(myDate.toString());
+    // console.log(myDate.toLocaleDateString());
+    // console.log(myDate.toLocaleTimeString())
+    // console.log(myDate.toDateString())
+
+  // let myCreatedDate = new Date("01-14-2023")
+   // let myCreatedDate = new Date(2026, 1, 26,5,3)
+   // console.log(myCreatedDate.toLocaleString())
+
+// let time = Date.now()
+// console.log(time)
+
+
