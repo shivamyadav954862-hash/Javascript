d175517a8dacbf2064346e59cdb663cca57e0764
Shivam yadav <shivamyadav984862@gmail.com>
2026-03-27 19:04:44 +0530

    iterations

---
+++ basics/01_basics/02_control_flow/iterations/while.js
@@ -0,0 +1,27 @@
+let index = 0;
+while (index<=10) {
+    // console.log(`Value of index is ${index}`)
+    index = index +2
+}
+
+let myArr  =["flash", "batman", "Superman"]
+let arr = 0
+while (arr <myArr.length ) {
+    // console.log(`value is ${myArr[arr]}`)
+    arr = arr +1
+}
+
+
+
+// let i = 20
+// do {
+//     console.log(`Score is ${i}`)
+//     i++
+// } while (i<=10);
+
+
+let i = 0
+do{
+console.log(`your marks is ${i}`)
+i++
+}while(i<=10)
